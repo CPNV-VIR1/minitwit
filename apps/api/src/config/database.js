377be1396d3646884ae4fa5e9b512c189a4cfee9
Samuel Michaud <samuel.michaud2000@gmail.com>
2023-03-09 10:46:07 +0100

    add tests create and findall posts

--- apps/api/src/config/database.js
+++ apps/api/src/config/database.js
@@ -1,11 +1,13 @@
-import { Sequelize } from "sequelize"
-import dotenv from "dotenv"
+import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
+
 dotenv.config()
 
 export const sequelize = new Sequelize({
-  dialect: "mysql",
+  dialect: 'mysql',
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  logging: false,
 })
