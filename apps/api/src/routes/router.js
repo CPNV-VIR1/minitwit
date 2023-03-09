2be16234c406a74a1351f0e5050b2567e6ad2194
Hyilbo <boris.shaffner@gmail.com>
2023-03-09 08:07:28 +0100

    Changed the router to support parameter passing in routes

--- apps/api/src/routes/router.js
+++ apps/api/src/routes/router.js
@@ -16,17 +16,27 @@ export default class Router {
       callback,
     })
   }
+
   run() {
     http
       .createServer((req, res) => {
+        let matchedRoute = null
         this.#routes.forEach((route) => {
-          if (
-            req.url.toLowerCase() === `${this.#prefix}${route.url.toLowerCase()}` &&
-            req.method.toLowerCase() === route.method.toLowerCase()
-          ) {
-            route.callback(req, res)
+          const urlPattern = `^${this.#prefix}${route.url.replace(/:\w+/g, "(\\w+)")}$`
+          const urlRegex = new RegExp(urlPattern, "i")
+          const matches = urlRegex.exec(req.url)
+          if (matches && req.method.toLowerCase() === route.method.toLowerCase()) {
+            matchedRoute = route
+            matchedRoute.params = matches.slice(1)
           }
         })
+
+        if (matchedRoute) {
+          matchedRoute.callback(req, res, ...matchedRoute.params)
+        } else {
+          res.writeHead(404, { "Content-Type": "text/plain" })
+          res.end("Not Found")
+        }
       })
       .listen(this.#port, () => console.log(`Server is running on port ${this.#port}`))
   }
