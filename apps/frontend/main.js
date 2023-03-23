3b328343072b87902bc83c68131d0280dd06ccc5
Hyilbo <boris.shaffner@gmail.com>
2023-03-23 10:33:29 +0100

    Add moment on packages and format messages date

--- apps/frontend/main.js
+++ apps/frontend/main.js
@@ -1,5 +1,6 @@
 import "./resources/css/style.css"
-import PostsServices from "./resources/services/posts.js";
+import PostsServices from "./resources/services/posts.js"
+import { dates } from "@cpnv/dates"
 
 const selectLanguage = document.querySelector(".header__language")
 let selectedLanguage = selectLanguage.value
@@ -16,32 +17,34 @@ const changeLanguage = async () => {
 
 const displayMessages = async () => {
   const messages = (await PostsServices.getAll()).reverse()
-  const divContent = document.querySelector('.messages__content')
-  divContent.innerHTML = ''
+  const divContent = document.querySelector(".messages__content")
+  divContent.innerHTML = ""
 
-  messages.forEach(message => {
-    const divMessage = document.createElement('div')
-    divMessage.classList.add('message')
-    divMessage.innerHTML = `<span class='timestamp'>${message.createdAt}</span> - ${message.content}`
+  messages.forEach((message) => {
+    const divMessage = document.createElement("div")
+    divMessage.classList.add("message")
+    divMessage.innerHTML = `<span class='timestamp'>${dates(message.createdAt).format("DD.MM - hh:mm")}</span> - ${
+      message.content
+    }`
     divContent.appendChild(divMessage)
   })
 }
 
 const createMessage = async (message) => {
-  await PostsServices.create({content: message})
-  messageInput.value = ''
+  await PostsServices.create({ content: message })
+  messageInput.value = ""
   await displayMessages()
 }
 
 await displayMessages()
 
-const btnSend = document.querySelector('.btn__send')
-const messageInput = document.querySelector('.message__input')
-btnSend.addEventListener('click', async () => {
+const btnSend = document.querySelector(".btn__send")
+const messageInput = document.querySelector(".message__input")
+btnSend.addEventListener("click", async () => {
   await createMessage(messageInput.value)
 })
-messageInput.addEventListener('keydown', async (e) => {
-  if (e.key === 'Enter') {
+messageInput.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
     await createMessage(messageInput.value)
   }
 })
